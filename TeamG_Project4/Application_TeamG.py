b24b50c28bcf4f695a04698c6606f90a702732bf
cassidyspen <cassidyspencer@sandiego.edu>
2022-05-11 20:36:52 -0700

    deleted old application

--- TeamG_Project4/Application_TeamG.py
+++ TeamG_Project4/Application_TeamG.py
@@ -39,38 +39,4 @@ root = Tk()
 root.title("Random Paint Application")
 root.geometry("500x300")
 app = PaintApp(root)
-<<<<<<< HEAD
 root.mainloop()
-=======
-root.mainloop()
-
-
-def PaintNumber(now):
-    now = LCG_TeamG.LCG_TeamG(now)
-    return (now%28)
-
-def PaintNumberList():
-    today = datetime.datetime.now()
-    now = today.second
-    numbers = []
-    for i in range (4):
-        now = LCG_TeamG.LCG_TeamG(now)
-        numbers.append(now)
-        i+=1
-    return numbers
-        
-def ColoringBook(paintnumbers):
-        print("Open your coloring book by using this link: https://jspaint.app/#local:27c2c8d383dbb")
-        num1 = PaintNumber(paintnumbers[0])
-        print("Your sky color is color number ", num1)
-        num2 = PaintNumber(paintnumbers[1])
-        print("Your ground color is color number ", num2)
-        num3 = PaintNumber(paintnumbers[2])
-        print("Your sun color is color number ", num3)
-        num4 = PaintNumber(paintnumbers[3])
-        print("Your tree color is color number ", num4)
-
-if __name__ == "__main__":
-    paintnumbers = PaintNumberList()
-    ColoringBook(paintnumbers)
->>>>>>> 7902bb3a31ba7161f55e12f7fee5f11264fb28b3
